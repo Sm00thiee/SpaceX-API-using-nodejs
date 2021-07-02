0cc8a13959da1c0ddfcd758dbf8462a17fd508ff
Nhan Luu <nhanlq.bi9178@st.usth.edu.vn>
2021-07-02 20:15:11 +0700

    Create schema for Launches

---
+++ models/launch.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+// image
+// flight number
+// name
+// details
+// date UTC.
+
+const Joi = require('Joi')
+
+const launchSchema = new mongoose.Schema({
+    image:{
+        small:{
+            type: String
+        },
+        large: {
+            type: String
+        }
+    },
+    flight_no: {
+        type: Number
+    },
+    name: {
+        type: String
+    },
+    details: {
+        type: String
+    },
+    date_utc: {
+        type: String
+    }
+})
+
+const Launch = mongoose.model('Launch', launchSchema)
+
+function validateLaunch(launch){
+    const schema = {
+        small: Joi.string(),
+        large: Joi.string(),
+        name: Joi.string(),
+        details: Joi.string(),
+        name: Joi.string(),
+        date_utc: Joi.string(),
+        flight_no: Joi.number().integer().positive()
+    }
+    return Joi.validate(launch, schema)
+}
+
+exports.Launch = Launch;
+exports.validate = validateLaunch;
